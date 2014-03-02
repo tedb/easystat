7565354ca13f3350306da0f05018732c7199e9f4
Ted Behling <ted@tedb.us>
2014-03-02 13:30:25 -0500

    big changes

--- easystat.go
+++ easystat.go
@@ -3,51 +3,62 @@ package easystat
 import (
 	"fmt"
 	"io"
-	"os"
 	"time"
 )
 
 // instantiable struct w/ OO style methods
 type Stats struct {
-	c    chan IncomingStat
-	// all access to the data map is wrapped in this mutex
-	mutex sync.RWMutex
-	data map[string]int64
+	c         chan IncomingStat
+	data      map[string]int64
+	w         io.Writer
+	started   time.Time
+	last_time time.Time
+	last_data map[string]int64
 }
 
 type IncomingStat struct {
-	key string
+	key   string
 	value int64
 }
 
-// return a new initialized Stats struct, and launch goroutine to read from channel and increment 
-func New() *Stats {
-	stats := &Stats{make(chan IncomingStat), make(map[string]int64)}
+// return a new initialized Stats struct, and launch goroutine to:
+// - read from channel and increment
+// - every interval, write stats to w, which could be a file, os.Stderr, or os.Stdout
+func NewWriter(w io.Writer, interval time.Duration) *Stats {
+	now := time.Now()
+	stats := &Stats{make(chan IncomingStat), make(map[string]int64), w, now, now, make(map[string]int64)}
 	go func() {
-		range incoming := range stats.c {
-			stats.mutex.Lock()
-			stats.data[incoming.key] += incoming.value
-			stats.mutex.Unlock()
+
+		ticker := time.NewTicker(interval)
+		for {
+			select {
+			case incoming, ok := <-stats.c:
+				if ok {
+					stats.data[incoming.key] += incoming.value
+				} else {
+					stats.write(time.Now())
+					ticker.Stop()
+					return
+				}
+			case now := <-ticker.C:
+				stats.write(now)
+			}
+
 		}
 	}()
 	return stats
 }
 
-// every interval, print a summary of the statistics to stdout
-func (s *Stats) PrintLoop(interval time.Duration) {
-	s.WriteLoop(os.Stdout, interval)
-}
+func (s *Stats) write(now time.Time) {
+	fmt.Fprintf(s.w, "%v %v\n", now, s.data)
 
-// every interval, write a summary of statistics to the io.Writer passed
-func (s *Stats) WriteLoop(w io.Writer, interval time.Duration) {
-	go func() {
-		ticker := time.Tick(interval)
-		for now := range ticker {
-			fmt.Fprintf(w, "%v %v\n", now, s.data)
-		}
-	}()
+	// copy current data into previous, so we can compare against it next time
+	s.last_time = now
+	for k, v := range s.data {
+		s.last_data[k] = v
+	}
 }
 
 func (s *Stats) Add(k string, v int64) {
-	s.c <- &IncomingStat{k, v}
+	s.c <- IncomingStat{k, v}
 }
